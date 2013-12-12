de21f8cbf66bed313a55d0165efea8963f55584c
Pride Mokhele <pride.mokhele@gmail.com>
2013-12-12 16:49:50 +0200

    * added social icons

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 //header('Location: coming-soon');
+$social_width = '50';
 ?>
+
 <img src="images/coming-soon-cover-animation.gif" width="850">
+<a href="http://behance.net/lonigraphics"><img src="images/behance.png" width="<?php echo $social_width?>"></a>
+<a href="http://za.linkedin.com/pub/lehlohonolo-makhokolo/82/4a3/a24"><img src="images/linkedin.png" width="<?php echo $social_width?>"></a>
+<a href="http://lonigraphics.tumblr.com/"><img src="images/tumblr.png" width="<?php echo $social_width?>"></a>
+<a href="https://twitter.com/lonigraphics"><img src="images/twitter.png" width="<?php echo $social_width?>"></a>
