8eabf60f3401740e873c46acd82534d51b1cf65a
Pride Mokhele <pride.mokhele@gmail.com>
2014-06-03 10:09:02 +0200

    * added social links

--- content/coming-soon/index.php
+++ content/coming-soon/index.php
@@ -1,4 +1,10 @@
 [page:Coming Soon]
 [template:empty]
 
-<img src="{templata:images}/loni/coming-soon-cover-animation.gif" width="800">
+<?php $social_width = 50;?>
+
+<img src="{templata:images}/coming-soon/coming-soon-cover-animation.gif" width="800">
+<a href="http://behance.net/lonigraphics"><img src="{templata:images}/coming-soon/behance.png" width="<?php echo $social_width?>"></a>
+<a href="http://za.linkedin.com/pub/lehlohonolo-makhokolo/82/4a3/a24"><img src="{templata:images}/coming-soon/linkedin.png" width="<?php echo $social_width?>"></a>
+<a href="http://lonigraphics.tumblr.com/"><img src="{templata:images}/coming-soon/tumblr.png" width="<?php echo $social_width?>"></a>
+<a href="https://twitter.com/lonigraphics"><img src="{templata:images}/coming-soon/twitter.png" width="<?php echo $social_width?>"></a>
